4ab1962acd645cd079e0c647b76c5d5428e9e71c
Mathias Buus <mathiasbuus@gmail.com>
2014-10-14 20:30:47 +0800

    rewrite

--- index.js
+++ index.js
@@ -1,48 +1,89 @@
-var document = require('global/document')
-var hg = require('mercury')
-var h = require('mercury').h
-var observify = require('observify')
 var path = require('path')
+var react = require('react')
+var events = require('events')
+var fs = require('fs')
+var defaultcss = require('defaultcss')
+var DOM = react.DOM
 
-module.exports = function(data) {
-  function TreeView() {
-    var state = observify(data)
-    return state
-  }
+var style = fs.readFileSync(__dirname+'/style.css', 'utf-8')
 
-  TreeView.render = function render(state) {
-    return renderTree(state)
-  }
-  
-  hg.app(document.body, TreeView(), TreeView.render)
-  
-  function renderTree(obj) {
-    return h('div.t-tree-container',
-      h('ul', obj['/'].map(function(child) {
-        if (child.type === 'file') return renderFile(child)
-        if (child.type === 'directory') return renderDirectory(child.path)
-      }))
-    )
-  }
+var Browser = react.createClass({
+  getInitialState: function() {
+    return {}
+  },
+  renderDirectory: function(dir, indent) {
+    var self = this
+    var entries = this.state[dir]
 
-  function renderFile(obj) {
-    return h('li', [
-      h('span.t-tree-icon#t-tree-expanded'),
-      h('span.t-tree-label', path.basename(obj.path))
-    ])
-  }
+    if (!entries) return DOM.ul(null)
 
-  function renderDirectory(dirPath) {
-    var obj = data[dirPath]
-    if (!obj) obj = []
-    var children = obj.map(function(child) {
-      if (child.type === 'file') return renderFile(child)
-      if (child.type === 'directory') return renderDirectory(child.path)
+    var list = entries.map(function(e) {
+      var name = path.basename(e.path)
+
+      var onfile = function() {
+        self.props.onfile(e)
+      }
+
+      if (e.type !== 'directory') return DOM.li({key:e.path}, DOM.span({className:'entry file', onClick:onfile}, name))
+
+      var open = self.state[e.path]
+
+      var onclick = function() {
+        if (open) self.props.onclose(e)
+        else self.props.onopen(e)
+      }
+
+      if (!open) return DOM.li({key:e.path}, DOM.span({className:'entry directory closed', onClick:onclick}, name))
+
+      return DOM.li({key:e.path},
+        DOM.span({className:'entry directory opened', onClick:onclick}, name),
+        self.renderDirectory(e.path)
+      )
     })
-    return h('li', [
-      h('span.t-tree-icon#t-tree-expanded'),
-      h('span.t-tree-label', path.basename(dirPath)),
-      h('ul', children)
-    ])
+
+    return DOM.ul(null, list)
+  },
+  render: function() {
+    return DOM.div({className:'tree-view'}, this.renderDirectory('/', 0))
   }
+})
+
+module.exports = function(opts) {
+  if (!opts) opts = {}
+
+  var that = new events.EventEmitter()
+  var state = {}
+  var comp
+
+  var onfile = function(e) {
+    that.emit('file', e.path, e)
+  }
+
+  var onopen = function(e) {
+    that.emit('directory', e.path, e)
+  }
+
+  var onclose = function(e) {
+    console.log('onclose', e)
+    state[e.path] = null
+    Object.keys(state).forEach(function(p) {
+      if (path.join(p, '/').indexOf(path.join(e.path, '/')) === 0) state[p] = null
+    })
+    if (comp) comp.setState(state)
+  }
+
+  that.style = style
+
+  that.directory = function(cwd, entries) {
+    state[cwd] = entries
+    if (comp) comp.setState(state)
+  }
+
+  that.appendTo = function(el) {
+    if (typeof el === 'string') el = document.querySelector(el)
+    if (opts.style !== false) defaultcss('tree-view', style)
+    comp = react.renderComponent(Browser({onopen:onopen, onclose:onclose, onfile:onfile}), el)
+  }
+
+  return that
 }
